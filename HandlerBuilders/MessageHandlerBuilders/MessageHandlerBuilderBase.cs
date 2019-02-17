b91b747cd4b9248bd5213702c0c0e28db2efa3e4
vova-lantsov-dev <vova.lantsov.25.private@gmail.com>
2019-02-17 15:21:07 +0200

    Refactoring: MessageHandlerBuilder base functionality moved to separate abstract class

---
+++ HandlerBuilders/MessageHandlerBuilders/MessageHandlerBuilderBase.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Telegram.Bot.Helper.Handlers;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace Telegram.Bot.Helper.HandlerBuilders.MessageHandlerBuilders
+{
+    public abstract class MessageHandlerBuilderBase<TLocalizationModel> where TLocalizationModel : class, new()
+    {
+        private protected readonly List<MessageExpressionHandler<TLocalizationModel>> _expressionList;
+        private protected readonly Func<MessageType, bool> _typePredicate;
+
+        private protected MessageHandlerBuilderBase(
+            List<MessageExpressionHandler<TLocalizationModel>> expressionList,
+            Func<MessageType, bool> typePredicate)
+        {
+            _expressionList = expressionList;
+            _typePredicate = typePredicate;
+            
+            Contains = new MessageTextPredicateHandlerBuilder<TLocalizationModel>(expressionList,
+                (messageText, text, comparison) => messageText.IndexOf(text, comparison) != -1,
+                typePredicate);
+            StartsWith = new MessageTextPredicateHandlerBuilder<TLocalizationModel>(expressionList,
+                (messageText, text, comparison) => messageText.StartsWith(text, comparison),
+                typePredicate);
+            EndsWith = new MessageTextPredicateHandlerBuilder<TLocalizationModel>(expressionList,
+                (messageText, text, comparison) => messageText.EndsWith(text, comparison),
+                typePredicate);
+        }
+
+        /// <summary>
+        /// Handler for text messages containing any of specific texts
+        /// </summary>
+        public readonly MessageTextPredicateHandlerBuilder<TLocalizationModel> Contains;
+
+        /// <summary>
+        /// Handler for text messages starting with any of specific texts
+        /// </summary>
+        public readonly MessageTextPredicateHandlerBuilder<TLocalizationModel> StartsWith;
+
+        /// <summary>
+        /// Handler for text messages ending with any of specific texts
+        /// </summary>
+        public readonly MessageTextPredicateHandlerBuilder<TLocalizationModel> EndsWith;
+
+        /// <summary>
+        /// Handler for text message
+        /// </summary>
+        /// <param name="text">Process if message text equals to specific value</param>
+        /// <param name="verified">Restrict access to this handler for specific verify values only</param>
+        /// <param name="comparison">Text comparison rule</param>
+        /// <returns></returns>
+        public Func<Message, TLocalizationModel, Task> this[string text, Verify verified = Verify.Unchecked, StringComparison comparison = StringComparison.Ordinal]
+        {
+            set
+            {
+                _expressionList.Add(new MessageExpressionHandler<TLocalizationModel>(
+                    m => (_typePredicate == null || _typePredicate(m.Type)) && m.Type == MessageType.Text &&
+                         m.Text.Equals(text, comparison), value, verified));
+            }
+        }
+
+        /// <summary>
+        /// Handlers for text message
+        /// </summary>
+        /// <param name="texts">Process if message text equals to specific value</param>
+        /// <param name="verified">Restrict access to these handlers for specific verify values only</param>
+        /// <param name="comparison">Text comparison rule</param>
+        /// <returns></returns>
+        public Func<Message, TLocalizationModel, Task> this[IEnumerable<string> texts, Verify verified = Verify.Unchecked, StringComparison comparison = StringComparison.Ordinal]
+        {
+            set
+            {
+                foreach (var text in texts)
+                    this[text, verified, comparison] = value;
+            }
+        }
+    }
+}
